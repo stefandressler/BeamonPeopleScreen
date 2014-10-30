bc5b2b891e79ae86a17ac775753b8dff14f06e92
Stefan Dressler <stefan.dressler@beamonconsulting.se>
2014-10-30 20:55:52 +0100

    initiate with empty service keys file

---
+++ app/scripts/BeamonPeople.Services.Keys.js
@@ -0,0 +1,14 @@
+var BeamonPeople = BeamonPeople || {};
+BeamonPeople.Services = BeamonPeople.Services || {};
+BeamonPeople.Services.Keys = {};
+
+
+/**
+ * Google Spreadsheet API Key
+ */
+BeamonPeople.Services.Keys.googleSpreadsheet = '';
+
+/**
+ * Instagram API Key
+ */
+BeamonPeople.Services.Keys.instagram = '';
